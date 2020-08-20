8371587e002a9fc855d12d6bb0a1979552c2ef60
Matheus Andrioli <matheus_andrioli@hotmail.com>
2020-08-19 23:09:05 -0300

    sparkles: Challenge finished

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -8,7 +8,7 @@ import CreateTransactionService from '../services/CreateTransactionService';
 import DeleteTransactionService from '../services/DeleteTransactionService';
 import ImportTransactionsService from '../services/ImportTransactionsService';
 
-import uploadConfig from '../config/upload';  
+import uploadConfig from '../config/upload';
 
 const upload = multer(uploadConfig);
 
@@ -48,14 +48,15 @@ transactionsRouter.delete('/:id', async (request, response) => {
   return response.status(204).send();
 });
 
-transactionsRouter.post('/import', 
+transactionsRouter.post(
+  '/import',
   upload.single('file'),
   async (request, response) => {
     const importTransactions = new ImportTransactionsService();
 
     const transactions = await importTransactions.execute(request.file.path);
 
-    return response.json(transaction);
+    return response.json(transactions); // s
   },
 );
 
